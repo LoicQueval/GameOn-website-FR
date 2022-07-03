1aace6b381486f624f59c7af74648678ab252510
queva <queval.loic@gmail.com>
2022-07-04 01:31:11 +0200

    add error message firstName for length and empty input, some clean

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const closeButton = document.querySelector('.close');
 const formData = document.querySelectorAll('.formData');
+const errorMessage = document.querySelector('.error');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
@@ -32,5 +33,26 @@ function closeModal() {
     console.log('close');
 }
 
+const firstName = document.forms['reserve']['first']
+const lastName = document.forms['reserve']['last']
 
 
+
+
+function validate() {
+    if (firstName.value === "") {
+        errorMessage.style.display = 'block';
+        firstName.focus();
+        firstName.style.border = '1px solid red';
+        errorMessage.innerHTML = "Vous devez entrer votre prénom";
+        return false;
+    }
+    if (firstName.value.length < 2) {
+        errorMessage.style.display = 'block';
+        firstName.focus();
+        firstName.style.border = '1px solid red';
+        errorMessage.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
+        return false
+    }
+
+}
