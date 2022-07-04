a6e687d6dea22c80ab630d89ffd076758815b047
queva <queval.loic@gmail.com>
2022-07-04 14:37:22 +0200

    add error message lastName for length and empty input, some clean add error message email for empty input and conforme email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-    var x = document.getElementById('myTopnav');
+    const x = document.getElementById('myTopnav');
     if (x.className === 'topnav') {
         x.className += ' responsive';
     } else {
@@ -11,8 +11,8 @@ function editNav() {
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const closeButton = document.querySelector('.close');
-const formData = document.querySelectorAll('.formData');
-const errorMessage = document.querySelector('.error');
+const errorMessage = document.querySelectorAll('.error');
+/*const formData = document.querySelectorAll('.formData');*/
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
@@ -30,29 +30,63 @@ if (closeButton !== null) {
 // close modal form
 function closeModal() {
     modalbg.style.display = 'none';
-    console.log('close');
 }
 
-const firstName = document.forms['reserve']['first']
-const lastName = document.forms['reserve']['last']
-
-
-
+const firstName = document.forms['reserve']['first'];
+const lastName = document.forms['reserve']['last'];
+const email = document.forms['reserve']['email']
 
 function validate() {
-    if (firstName.value === "") {
-        errorMessage.style.display = 'block';
+
+    if (firstName.value === '') {
         firstName.focus();
         firstName.style.border = '1px solid red';
-        errorMessage.innerHTML = "Vous devez entrer votre prénom";
-        return false;
-    }
-    if (firstName.value.length < 2) {
-        errorMessage.style.display = 'block';
+        errorMessage[0].style.display = 'block';
+        errorMessage[0].innerHTML = 'Vous devez entrer votre prénom';
+    } else if (firstName.value.length < 2) {
         firstName.focus();
         firstName.style.border = '1px solid red';
-        errorMessage.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
-        return false
+        errorMessage[0].style.display = 'block';
+        errorMessage[0].innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
+    } else {
+        firstName.style.border = '0px solid red';
+        errorMessage[0].style.display = 'none';
     }
 
+    if (lastName.value === '') {
+        lastName.focus();
+        lastName.style.border = '1px solid red';
+        errorMessage[1].style.display = 'block';
+        errorMessage[1].innerHTML = 'Vous devez entrer votre nom';
+    } else if (lastName.value.length < 2) {
+        lastName.focus();
+        lastName.style.border = '1px solid red';
+        errorMessage[1].style.display = 'block';
+        errorMessage[1].innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
+    } else {
+        lastName.style.border = '0px solid red';
+        errorMessage[1].style.display = 'none';
+    }
+
+    if (email.value === '') {
+        email.focus();
+        email.style.border = '1px solid red';
+        errorMessage[2].style.display = 'block';
+        errorMessage[2].innerHTML = 'Veuillez entrer un email';
+    } else if (checkEmail(email.value)) {
+        email.style.border = '0px solid red';
+        errorMessage[2].style.display = 'none';
+    } else {
+        email.focus();
+        email.style.border = '1px solid red';
+        errorMessage[2].style.display = 'block';
+        errorMessage[2].innerHTML = 'Veuillez entrer un email valide';
+    }
+
+    function checkEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    return false
 }
